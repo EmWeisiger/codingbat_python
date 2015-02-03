abfffd89b51a0496fbc3ac1c5034e2cfeb0360e9
Em Weisiger <emweisiger@gmail.com>
2015-02-02 19:29:20 -0700

    Logic_2.py
    My solutions to the problem set of Logic-2 in python

---
+++ Logic_2.py
@@ -0,0 +1,119 @@
+"""
+We want to make a row of bricks that is goal inches long. We have a number of small bricks (1 inch each) and big bricks (5 inches each). Return True if it is possible to make the goal by choosing from the given bricks. This is a little harder than it looks and can be done without any loops.
+
+make_bricks(3, 1, 8) → True
+make_bricks(3, 1, 9) → False
+make_bricks(3, 2, 10) → True
+"""
+
+def make_bricks(small, big, goal):
+  goal -= 5*min(big,goal/5)
+  return goal-small <= 0
+
+"""
+Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum. 
+
+lone_sum(1, 2, 3) → 6
+lone_sum(3, 2, 3) → 2
+lone_sum(3, 3, 3) → 0
+"""
+
+def lone_sum(a, b, c):
+  sum = a + b + c
+  if (a == b == c):
+    return sum - (a + b + c)
+  elif (a == b):
+    return sum - (a + b)
+  elif (a == c):
+    return sum - (a + c)
+  elif (b == c):
+    return sum - (b + c)
+  elif (a == b == c):
+    return sum - (a + b + c)
+  else:
+    return sum
+
+"""
+ALTERNATE SOLUTION
+"""
+
+def lone_sum(a, b, c):
+  sum = 0
+  if a != b and a != c: sum += a
+  if b != a and b != c: sum += b
+  if c != a and c != b: sum += c
+  
+  return sum
+
+"""
+Given 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and values to its right do not count. So for example, if b is 13, then both b and c do not count. 
+
+lucky_sum(1, 2, 3) → 6
+lucky_sum(1, 2, 13) → 3
+lucky_sum(1, 13, 3) → 1
+"""
+
+def lucky_sum(a, b, c):
+  sum = (a + b + c)
+  if (a == 13 and b == 13) or (a == 13 and c ==13):
+    return sum - (a + b + c)
+  elif a == 13:
+    return sum - (a + b + c)
+  elif b == 13:
+    return sum - (b + c)
+  elif c == 13:
+    return sum - c
+  else:
+    return sum
+
+"""
+Given 3 int values, a b c, return their sum. However, if any of the values is a teen -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that takes in an int value and returns that value fixed for the teen rule. In this way, you avoid repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at the same indent level as the main no_teen_sum(). 
+
+no_teen_sum(1, 2, 3) → 6
+no_teen_sum(2, 13, 1) → 3
+no_teen_sum(2, 1, 14) → 3
+"""
+
+def no_teen_sum(a, b, c):
+  def fix_teen(n):
+    return n if n not in [13,14,17,18,19] else 0
+  return fix_teen(a)+fix_teen(b)+fix_teen(c)
+
+"""
+For this problem, we'll round an int value up to the next multiple of 10 if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of their rounded values. To avoid code repetition, write a separate helper "def round10(num):" and call it 3 times. Write the helper entirely below and at the same indent level as round_sum(). 
+
+round_sum(16, 17, 18) → 60
+round_sum(12, 13, 14) → 30
+round_sum(6, 4, 4) → 10
+"""
+
+def round_sum(a, b, c):
+  def round10(num):
+    return (num+5)/10*10
+  return round10(a)+round10(b)+round10(c)
+
+"""
+ALTERNATE SOLUTION
+"""
+
+def round_sum(a, b, c):
+  return round10(a) + round10(b) + round10(c)
+  
+def round10(num):
+  mod = num % 10
+  num -= mod
+  if mod >= 5: num += 10
+  return num
+
+"""
+Given three ints, a b c, return True if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: abs(num) computes the absolute value of a number. 
+
+close_far(1, 2, 10) → True
+close_far(1, 2, 3) → False
+close_far(4, 1, 3) → True
+"""
+
+def close_far(a, b, c):
+  return (abs(abs(b)-abs(c))>=2) and \
+  ((abs(abs(b)-abs(a))<=1 and abs(abs(c)-abs(a))>=2) \
+  or (abs(abs(c)-abs(a))<=1 and abs(abs(b)-abs(a))>=2))
