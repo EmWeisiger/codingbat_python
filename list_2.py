ed115e1389873784d84909da4688e5eb3b32bded
Em Weisiger <emweisiger@gmail.com>
2015-02-05 14:44:10 -0700

    Update list_2.py

--- list_2.py
+++ list_2.py
@@ -20,3 +20,42 @@ big_diff([10, 3, 5, 6]) → 7
 big_diff([7, 2, 10, 9]) → 8
 big_diff([2, 10, 7, 2]) → 8
 """
+
+def big_diff(nums):
+  return max(nums) - min(nums)
+
+"""
+Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more. 
+
+centered_average([1, 2, 3, 4, 100]) → 3
+centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+centered_average([-10, -4, -2, -4, -2, 0]) → -3
+"""
+
+def centered_average(nums):
+  nums.sort()
+  return sum(nums[1:-1])/(len(nums)-2)
+  
+"""
+Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count. 
+
+sum13([1, 2, 2, 1]) → 6
+sum13([1, 1]) → 2
+sum13([1, 2, 2, 1, 13]) → 6
+"""
+
+def sum13(nums):
+  while 13 in nums:
+    if nums.index(13) < len(nums)-1:
+      nums.pop(nums.index(13)+1)
+    nums.pop(nums.index(13))
+  return sum(nums)
+  
+"""
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers. 
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+"""
+
